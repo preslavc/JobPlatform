cf58be4ac3654cfa120335a1e514f745f1149656
Preslav Tsenov <preslavc@gmail.com>
2020-03-23 21:46:00 +0200

    - Remove 13 symbol EIK validation - Add EikValidation tests

---
+++ Tests/JobPlatform.Services.Data.Tests/EikValidationAttributeTests.cs
@@ -0,0 +1,36 @@
+﻿namespace JobPlatform.Services.Data.Tests
+{
+    using JobPlatform.Web.Infrastructure.ValidationAttributes;
+    using Xunit;
+
+    public class EikValidationAttributeTests
+    {
+        [Theory]
+        [InlineData("000000000")]
+        [InlineData("160026043")]
+        [InlineData("115904389")]
+        [InlineData("400004390")]
+        public void ValidEik(string eik)
+        {
+            EikValidationAttribute attribute = new EikValidationAttribute();
+            bool value = attribute.IsValid(eik);
+            Assert.True(value);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("1234as789")]
+        [InlineData("abcdefghi")]
+        [InlineData("         ")]
+        [InlineData("\0")]
+        [InlineData("00000000")]
+        [InlineData("0000000000")]
+        public void InvalidEik(string eik)
+        {
+            EikValidationAttribute attribute = new EikValidationAttribute();
+            bool value = attribute.IsValid(eik);
+            Assert.False(value);
+        }
+    }
+}
--- Web/JobPlatform.Web.Infrastructure/ValidationAttributes/EikValidationAttribute.cs
+++ Web/JobPlatform.Web.Infrastructure/ValidationAttributes/EikValidationAttribute.cs
@@ -9,23 +9,18 @@
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            string eik = value.ToString();
-            if (!Regex.IsMatch(eik, "^(?=[0-9]*$)(?:.{9}|.{13})$"))
+            if (value == null)
             {
                 return new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
             }
 
-            if (eik.Length == 9)
+            string eik = value.ToString();
+            if (!Regex.IsMatch(eik, "^[0-9]{9}$"))
             {
-                return this.CalculateNineDigitEik(eik) ? ValidationResult.Success : new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
+                return new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
             }
 
-            if (eik.Length == 13)
-            {
-                return this.CalculateThirteenDigitEik(eik) ? ValidationResult.Success : new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
-            }
-
-            return new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
+            return this.CalculateNineDigitEik(eik) ? ValidationResult.Success : new ValidationResult(GlobalConstants.ErrorMessageInvalidEik);
         }
 
         private bool CalculateNineDigitEik(string eik)
@@ -50,34 +45,6 @@
 
             remainder = (checksum % 11) % 10;
             return remainder == eik[8] - '0';
-        }
-
-        private bool CalculateThirteenDigitEik(string eik)
-        {
-            int[] weight1 = new int[] { 2, 7, 3, 5 };
-            int[] weight2 = new int[] { 4, 9, 5, 7 };
-
-            int checksum = 0;
-
-            for (int i = 8; i < 12; i++)
-            {
-                checksum += (eik[i] - '0') * weight1[i - 8];
-            }
-
-            int remainder = checksum % 11;
-            if (remainder != 10)
-            {
-                return remainder == eik[12] - '0';
-            }
-
-            checksum = 0;
-            for (int i = 8; i < 12; i++)
-            {
-                checksum += (eik[i] - '0') * weight2[i - 8];
-            }
-
-            remainder = (checksum % 11) % 10;
-            return remainder == eik[12] - '0';
         }
     }
 }
