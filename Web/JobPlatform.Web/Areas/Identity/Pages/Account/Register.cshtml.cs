4fac12465e8a9af59f4e7adcc1766ded8b5335c2
Preslav Tsenov <preslavc@gmail.com>
2020-03-26 17:40:10 +0200

    - Add more fields in user register.

--- Web/JobPlatform.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/JobPlatform.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -48,20 +48,32 @@ namespace JobPlatform.Web.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            [StringLength(50, ErrorMessage = "{0} трябва да е с минимална дължина {2} или максимална {1} ", MinimumLength = 3)]
+            [Display(Name = "Име")]
+            public string FirstName { get; set; }
+
+            [StringLength(50, ErrorMessage = "{0} трябва да е с минимална дължина {2} или максимална {1} ", MinimumLength = 3)]
+            [Display(Name = "Фамилия")]
+            public string LastName { get; set; }
+
+            [Display(Name = "Телефон за връзка")]
+            [Phone]
+            public string PhoneNumber { get; set; }
+
             [Required(ErrorMessage = GlobalConstants.ErrorMessageRequiredField)]
             [EmailAddress(ErrorMessage = GlobalConstants.ErrorMessageEmailField)]
-            [Display(Name = "Имейл адрес")]
+            [Display(Name = "Имейл адрес *")]
             public string Email { get; set; }
 
             [Required(ErrorMessage = GlobalConstants.ErrorMessageRequiredField)]
             [MinLength(6, ErrorMessage = "Минималната дължина трябва да е {1} символа")]
             [MaxLength(100, ErrorMessage = "Максимална дължина трябва да е {1} символа")]
             [DataType(DataType.Password)]
-            [Display(Name = "Парола")]
+            [Display(Name = "Парола *")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Потвърдете паролата")]
+            [Display(Name = "Потвърдете паролата *")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
@@ -78,7 +90,12 @@ namespace JobPlatform.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    PhoneNumber = Input.PhoneNumber,
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
